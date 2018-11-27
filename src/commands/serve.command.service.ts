618113fe737de3f785f846bb29fc540851f2315b
marcus-sa <esetmarcussharkgamingcomputer@gmail.com>
2018-11-27 11:32:35 +0100

    feat(serve) add port as an option

--- src/commands/serve.command.service.ts
+++ src/commands/serve.command.service.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@one/core';
 import { Command as Options } from 'commander';
 import { Command } from './command.decorator';
 
@@ -5,15 +6,25 @@ import { Input } from '../models';
 import { ServeAction } from '../actions';
 import { AbstractCommand } from './abstract.command';
 
-@Command(ServeAction)
+// @Command(ServeAction)
+@Injectable()
 export class ServeCommand extends AbstractCommand {
+  constructor(public readonly action: ServeAction) {
+    super();
+  }
+
   public load() {
     this.program
       .command('serve [project] [browser]')
       .alias('s')
       .description('Compile extensions and start a dev server')
       .option('--hmr', 'Use Hot Module Replacement')
+      .option('--port [port]', 'Port to use for dev server')
       .action(async (project: string, browser: string, command: Options) => {
+        if (command.cwd) {
+          this.workspace.setCwd(command.cwd);
+        }
+
         await this.workspace.validate();
 
         const inputs: Input[] = [];
@@ -22,6 +33,7 @@ export class ServeCommand extends AbstractCommand {
 
         const options: Input[] = [];
         options.push({ name: 'hmr', value: !!command.hmr });
+        options.push({ name: 'port', value: Number(command.port) });
 
         await this.action.handle(inputs, options);
       });
