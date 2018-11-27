92dce23bd606dec47d158bf7b2cceb1ed9d90280
marcus-sa <esetmarcussharkgamingcomputer@gmail.com>
2018-11-27 11:30:50 +0100

    feat: add proper find fn

--- src/util/find-up.ts
+++ src/util/find-up.ts
@@ -1,6 +1,18 @@
 import * as path from 'path';
 import * as fs from 'fs-extra';
 
+export async function find(names: string[], from: string) {
+  for (const name of names) {
+    const p = path.join(from, name);
+
+    if (await fs.pathExists(p)) {
+      return p;
+    }
+  }
+
+  return null;
+}
+
 export async function findUp(names: string | string[], from: string) {
   if (!Array.isArray(names)) {
     names = [names];
--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,6 @@
 export * from './find-up';
 export * from './snakerize';
 export * from './rename-property';
+export * from './filter-async';
 
 export const toArray = <T>(arr: T | T[]): T[] => !Array.isArray(arr) ? [arr] : arr;
